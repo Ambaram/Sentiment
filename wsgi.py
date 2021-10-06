6587721b35b3a992563de61b52e233e40efb6282
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-10-07 02:06:48 +0530

    gunicorn issues

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
-from app import app as application
+from app import app
 import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, "/root/customer-account-automation/")
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
